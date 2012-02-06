a9016092dd301eee806d24de9d1921c92d5b3ad8
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2012-02-06 22:09:02 +0000

    Notes de la Partie II pas mal complétées. Quelques fichiers ont changé de nom.

---
+++ simulation/montecarlo.R
@@ -0,0 +1,98 @@
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
+
+###
+### EXEMPLE 9.1
+###
+
+## Estimations successives de l'intégrale par la méthode Monte
+## Carlo avec des échantillons de plus en plus grands.
+f <- function(x) x^2.2 * exp(-x/10)
+x <- runif(1e2, 2, 5)
+3 * mean(f(x))
+x <- runif(1e3, 2, 5)
+3 * mean(f(x))
+x <- runif(1e4, 2, 5)
+3 * mean(f(x))
+x <- runif(1e5, 2, 5)
+3 * mean(f(x))
+x <- runif(1e6, 2, 5)
+3 * mean(f(x))
+
+## On trace des graphiques de la vraie fonction à intégrer et
+## de trois évaluations par la méthode Monte Carlo.
+op <- par(mfrow = c(2, 2)) # 4 graphiques en grille 2 x 2
+curve(f(x), xlim = c(2, 5), lwd = 2, main = "Vraie fonction")
+
+x <- runif(1e2, 2, 5)
+plot(x, f(x), main = "n = 100",
+     pch = 21, bg = "lightblue")
+
+x <- runif(1e3, 2, 5)
+plot(x, f(x), main = "n = 1000",
+     pch = 21, bg = "lightblue")
+
+x <- runif(1e4, 2, 5)
+plot(x, f(x), main = "n = 10 000",
+     pch = 21, bg = "lightblue")
+
+par(op)                    # revenir aux paramètres par défaut
+
+###
+### EXEMPLE 9.2
+###
+
+## Estimations successives de l'intégrale double par la
+## méthode Monte Carlo avec des échantillons de plus en plus
+## grands.
+u <- runif(1e2, 0, 1.25)
+v <- runif(1e2, 0, 1.25)
+mean(sqrt(4 - u^2 - v^2)) * 1.25^2
+u <- runif(1e3, 0, 1.25)
+v <- runif(1e3, 0, 1.25)
+mean(sqrt(4 - u^2 - v^2)) * 1.25^2
+u <- runif(1e4, 0, 1.25)
+v <- runif(1e4, 0, 1.25)
+mean(sqrt(4 - u^2 - v^2)) * 1.25^2
+
+## Graphiques de la vraie fonction et des points où celle-ci
+## est évaluée avec la méthode Monte Carlo. Pour faire un
+## graphique en trois dimensions, on peut utiliser la fonction
+## 'persp'.
+op <- par(mfrow = c(2, 2), mar = c(1, 1, 2, 1))
+f <- function(x, y) sqrt(4 - x^2 - y^2)
+x <- seq(0, 1.25, length = 25)
+y <- seq(0, 1.25, length = 25)
+persp(x, y, outer(x, y, f), main = "Vraie fonction",
+      zlim = c(0, 2), theta = 120, phi = 30, col = "lightblue",
+      zlab = "z", ticktype = "detailed")
+
+## Pour faire les trois autres graphiques, nous allons d'abord
+## créer des graphiques en trois dimensions vides, puis y
+## ajouter des points avec la fonction 'points'. La fonction
+## 'trans3d' sert à convertir les coordonnées des points dans
+## la projection utilisée par 'persp'.
+u <- runif(1e2, 0, 1.25)
+v <- runif(1e2, 0, 1.25)
+res <- persp(x, y, matrix(NA, length(x), length(y)), main = "n = 100",
+             zlim = c(0, 2), theta = 120, phi = 30,
+             zlab = "z", ticktype = "detailed")
+points(trans3d(u, v, f(u, v), pm = res),
+       pch = 21, bg = "lightblue")
+
+u <- runif(1e3, 0, 1.25)
+v <- runif(1e3, 0, 1.25)
+res <- persp(x, y, matrix(NA, length(x), length(y)), main = "n = 1000",
+             zlim = c(0, 2), theta = 120, phi = 30,
+             zlab = "z", ticktype = "detailed")
+points(trans3d(u, v, f(u, v), pm = res),
+       pch = 21, bg = "lightblue")
+
+u <- runif(1e4, 0, 1.25)
+v <- runif(1e4, 0, 1.25)
+res <- persp(x, y, matrix(NA, length(x), length(y)), main = "n = 10 000",
+             zlim = c(0, 2), theta = 120, phi = 30,
+             zlab = "z", ticktype = "detailed")
+points(trans3d(u, v, f(u, v), pm = res),
+       pch = 21, bg = "lightblue")
+
+par(op)                    # revenir aux paramètres par défaut
---
+++ simulation/simulation.R
@@ -0,0 +1,296 @@
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
+
+###
+### EXEMPLE 8.1
+###
+
+## Simulation un échantillon aléatoire d'une distribution
+## exponentielle par la méthode de l'inverse.
+lambda <- 5
+x <- -log(runif(1000))/lambda
+
+## Pour faire une petite vérification, on va tracer
+## l'histogramme de l'échantillon et y superposer la véritable
+## densité d'une exponentielle de paramètre lambda. Les deux
+## graphiques devraient concorder.
+
+## Tracé de l'histogramme. Il faut spécifier l'option 'prob =
+## TRUE' pour que l'axe des ordonnées soit gradué en
+## probabilités plutôt qu'en nombre de données. Sinon, le
+## graphique de la densité que l'on va ajouter dans un moment
+## n'apparaîtra pas sur le graphique.
+hist(x, prob = TRUE) # histogramme gradué en probabilités
+
+## Pour ajouter la densité, on a la très utile fonction
+## curve() pour tracer une fonction f(x) quelconque. Avec
+## l'option 'add = TRUE', le graphique est ajouté au graphique
+## existant.
+curve(dexp(x, rate = lambda), add = TRUE)
+
+###
+### EXEMPLE 8.4
+###
+
+## On trouvera ci-dessous une mise en oeuvre de l'algorithme
+## d'acceptation-rejet pour simuler des observations d'une
+## distribution Bêta(3, 2). La procédure est intrinsèquement
+## itérative, alors nous devons utiliser des boucles. Il y
+## aurait plusieurs manières de faire, j'ai décidé de placer
+## une boucle 'repeat' à l'intérieur d'une boucle 'for'.
+rbeta.ac <- function(n)
+{
+    x <- numeric(n)        # initialisation du contenant
+    g <- function(x)       # fonction enveloppante
+        if (x < 0.8) 2.5 * x else 10 - 10 * x
+    G <- function(x)       # son intégrale
+        if (x < 0.8) sqrt(0.8 * x) else 1 - sqrt(0.2 - 0.2 * x)
+
+    for (i in seq_len(n))  # il faut simuler 'n' nombres
+    {
+        repeat   # on peut devoir s'y reprendre plusieurs fois
+        {
+            y <- G(runif(1))
+            if (1.2 * g(y) * runif(1) <= dbeta(y, 3, 2))
+            {
+                x[i] <- y
+                break      # sortir de la boucle repeat
+            }
+        }
+    }
+    x                                   # retourner x
+}
+
+## Vérification empirique pour voir si ça marche.
+x <- rbeta.ac(1000)
+hist(x, prob = TRUE)
+curve(dbeta(x, 3, 2), add = TRUE)
+
+###
+### FONCTIONS DE SIMULATION DANS R
+###
+
+## La fonction de base pour simuler des nombres uniformes est
+## 'runif'.
+runif(10)                  # sur (0, 1) par défaut
+runif(10, 2, 5)            # sur un autre intervalle
+2 + 3 * runif(10)          # équivalent, moins lisible
+
+## R est livré avec plusieurs générateurs de nombres
+## aléatoires. On peut en changer avec la fonction 'RNGkind'.
+RNGkind("Wichmann-Hill")   # générateur de Excel
+runif(10)                  # rien de particulier à voir
+RNGkind("default")         # retour au générateur par défaut
+
+## La fonction 'set.seed' est très utile pour spécifier
+## l'amorce d'un générateur. Si deux simulations sont
+## effectuées avec la même amorce, on obtiendra exactement les
+## mêmes nombres aléatoires et, donc, les mêmes résultats.
+## Très utile pour répéter une simulation à l'identique.
+set.seed(1)                # valeur sans importance
+runif(5)                   # 5 nombres aléatoires
+runif(5)                   # 5 autres nombres
+set.seed(1)                # réinitialisation de l'amorce
+runif(5)                   # les mêmes 5 nombres que ci-dessus
+
+## Plutôt que de devoir utiliser la méthode de l'inverse ou un
+## autre algorithme de simulation pour obtenir des nombres
+## aléatoires d'une loi de probabilité non uniforme, R fournit
+## des fonctions de simulation pour bon nombre de lois. Toutes
+## ces fonctions sont vectorielles. Ci-dessous, P == Poisson
+## et G == Gamma pour économiser sur la notation.
+n <- 10                    # taille des échantillons
+rbinom(n, 5, 0.3)          # Binomiale(5, 0,3)
+rbinom(n, 1, 0.3)          # Bernoulli(0,3)
+rnorm(n)                   # Normale(0, 1)
+rnorm(n, 2, 5)             # Normale(2, 25)
+rpois(n, c(2, 5))          # P(2), P(5), P(2), ..., P(5)
+rgamma(n, 3, 2:11)         # G(3, 2), G(3, 3), ..., G(3, 11)
+rgamma(n, 11:2, 2:11)      # G(11, 2), G(10, 3), ..., G(2, 11)
+
+## La fonction 'sample' sert pour simuler d'une distribution
+## discrète quelconque. Le premier argument est le support de
+## la distribution et le second, la taille de l'échantillon
+## désirée. Par défaut, l'échantillonnage se fait avec remise
+## et avec des probabilités égales sur tout le support.
+sample(1:49, 7)            # numéros pour le 7/49
+sample(1:10, 10)           # mélange des nombres de 1 à 10
+
+## On peut échantilloner avec remise.
+sample(1:10, 10, replace = TRUE)
+
+## On peut aussi spécifier une distribution de probabilités
+## non uniforme.
+x <- sample(c(0, 2, 5), 1000, prob = c(2, 5, 3)/10)
+table(x)                   # tableau de fréquences
+
+###
+### AUTRES TECHNIQUES DE SIMULATION
+###
+
+## MÉLANGES CONTINUS
+
+## Un mélange continu de deux variables aléatoires est créé
+## lorsque l'on suppose qu'un paramètre d'une distribution f
+## est une réalisation d'une autre variable aléatoire avec
+## densité u, comme ceci:
+##
+## X|Theta = theta ~ f(x|theta)
+##           Theta ~ u(theta)
+##
+## Ce genre de modèle est fréquent en analyse bayesienne et
+## souvent utilisé en actuariat. Certaines lois de probabilité
+## sont aussi uniquement définies en tant que mélanges.
+##
+## L'intérêt, ici, est d'obtenir des observations de la
+## variable aléatoire non conditionnelle X. L'algorithme de
+## simulation est simple:
+##
+## 1. Simuler un nombre theta de la distribution de Theta.
+## 2. Simuler une valeur x de la distribution de X|Theta = theta.
+##
+## Ce qu'il importe de remarquer dans l'algorithme ci-dessus,
+## c'est que le paramètre de mélange (theta) change pour
+## chaque observation simulée. Autrement il n'y a juste pas de
+## mélange, on obtient simplement un échantillon de la
+## distribution f(x|theta).
+##
+## Les mélanges continus sont simples à faire en R puisque les
+## fonctions de simulation sont vectorielles. Par exemple,
+## simulons 1000 observations du mélange
+##
+## X|Theta = theta ~ Poisson(theta)
+##           Theta ~ Gamma(5, 4)
+theta <- rgamma(1000, 5, 4) # 1000 paramètres de mélange...
+x <- rpois(1000, theta)     # ... pour 1000 Poisson différentes
+
+## On peut écrire le tout en une seule expression.
+x <- rpois(1000, rgamma(1000, 5, 4))
+
+## On peut démontrer que la distribution non conditionnelle de
+## X est une binomiale négative de paramètres 5 et 4/(4 + 1) =
+## 0,8. Faisons une vérification empirique. On calcule d'abord
+## le tableau de fréquences des observations de l'échantillon
+## avec la fonction 'table'. Il existe une méthode de 'plot'
+## pour les tableaux de fréquences.
+(p <- table(x))            # tableau de fréquences
+plot(p/length(x))          # graphique
+
+## On ajoute au graphique les masses de probabilités théoriques.
+(xu <- unique(x))          # valeurs distinctes de x
+points(xu, dnbinom(xu, 5, 0.8), pch = 21, bg = "red3")
+
+## MÉLANGES DISCRETS
+
+## Le mélange discret est un cas limite du mélange continu
+## lorsque la distribution de Theta est une Bernoulli de
+## paramètre p, c'est-à-dire que Pr[Theta = 1] = p. Le
+## résultat du mélange est une distribution avec densité de la
+## forme
+##
+## f(x) = p f1(x) + (1 - p) f2(x),
+##
+## où f1 et f2 sont deux densités quelconques. Les mélanges
+## discrets sont très souvent utilisés pour créer de nouvelles
+## distributions aux caractéristiques particulières que l'on
+## ne retrouve pas chez les distributions d'usage courant.
+##
+## Par exemple, le mélange suivant de deux distributions
+## log-normales résulte en une fonction de densité de
+## probabilité bimodale, mais ayant néanmoins une queue
+## similaire à celle d'une log-normale. Graphiquement:
+op <- par(mfrow = c(3, 1)) # trois graphiques superposés
+curve(dlnorm(x, 3.575, 0.637),
+      xlim = c(0, 250),  ylab = "f(x)",
+      main = expression(paste("Log-normale(",
+          mu, " = 3,575, ", sigma, " = 0,637)")))
+curve(dlnorm(x, 4.555, 0.265),
+      xlim = c(0, 250), ylab = "f(x)",
+      main = expression(paste("Log-normale(",
+          mu, " = 4,555, ", sigma, " = 0,265)")))
+curve(0.554 * dlnorm(x, 3.575, 0.637) +
+      0.446 * dlnorm(x, 4.555, 0.265),
+      xlim = c(0, 250),  ylab = "f(x)",
+      main = expression(paste("Mélange (p = 0,554)")))
+par(op)                    # revenir aux paramètres par défaut
+
+## L'algorithme de simulation d'un mélange discret est
+##
+## 1. Simuler un nombre u uniforme sur (0, 1).
+## 2. Si u <= p, simuler un nombre de f1(x), sinon simuler de
+##    f2(x).
+##
+## La clé pour simuler facilement d'un mélange discret en R:
+## réaliser que l'ordre des observations est sans importance
+## et, donc, que l'on peut simuler toutes les observations de
+## la première loi, puis toutes celles de la seconde loi. La
+## seule chose dont il reste à tenir compte: le nombre
+## d'observations qui provient de chaque loi; pour chaque
+## observation, la probabilité qu'elle provienne de la
+## première loi est p.
+##
+## Ici, on veut simuler des observations d'un mélange discret
+## de deux lois log-normales, l'une de paramètres 3,575 et
+## 0,637, l'autre de paramètres 4,555 et 0,265. Le paramètre
+## de mélange est p = 0,554.
+n <- 1000                  # taille de l'échantillon
+n1 <- rbinom(1, n, 0.554)  # quantité provenant de la loi 1
+x <- c(rlnorm(n1, 3.575, 0.637),     # observations de loi 1
+       rlnorm(n - n1, 4.555, 0.265)) # observations de loi 2
+hist(x, prob = TRUE)                 # histogramme
+curve(0.554 * dlnorm(x, 3.575, 0.637) +
+      0.446 * dlnorm(x, 4.555, 0.265),
+      add = TRUE, lwd = 2, col = "red3") # densité théorique
+
+## CONVOLUTIONS
+
+## Une convolution est la somme de deux variables aléatoires
+## indépendantes. De manière générale, une convolution est
+## très compliquée à évaluer, même numériquement. Certaines
+## convolutions sont toutefois bien connues:
+##
+## 1. une somme de Bernoulli est une binomiale;
+## 2. une somme de géométriques est une binomiale négative;
+## 3. une somme d'exponentielles est une gamma;
+## 4. une somme de Poisson est une Poisson;
+## 5. une somme de normales est une normale, etc.
+##
+## On peut utiliser ces résultats pour simuler des
+## observations de certaines lois.
+##
+## Par exemple, pour simuler une observation d'une
+## distribution Gamma(alpha, lambda), on peut sommer alpha
+## observations d'une Exponentielle(lambda). Ces dernières
+## sont obtenues par la méthode de l'inverse.
+alpha <- 5
+lambda <- 2
+- sum(log(runif(alpha)))/lambda # une observation de la gamma
+
+## Pour simuler un échantillon de taille n de la gamma, il
+## faut simuler n * alpha observations de l'exponentielle. Il
+## existe des algorithmes plus efficaces pour simuler d'une
+## loi gamma.
+n <- 1000                  # taille de l'échantillon
+x <- - rowSums(matrix(log(runif(n * alpha)),
+                      nrow = n))/lambda
+hist(x, prob = TRUE)       # histogramme
+curve(dgamma(x, alpha, lambda),
+      add = TRUE, lwd = 2, col = "red3") # densité théorique
+
+## La simulation peut aussi servir à estimer des
+## caractéristiques de la distribution d'une convolution
+## difficiles à calculer explicitement. Par exemple, supposons
+## que l'on a
+##
+## X ~ Gamma(3, 4)
+## Y ~ Gamma(5, 2)
+##
+## et que l'on souhaite calculer le 95e centile de X + Y. Il
+## n'y a pas de solution explicite pour la distribution de X +
+## Y puisque les deux lois gamma n'ont pas le même paramètre
+## d'échelle (lambda). Procédons donc par simulation pour
+## obtenir une approximation du résultat.
+x <- rgamma(10000, 3, 4)   # échantillon de la première loi
+y <- rgamma(10000, 5, 2)   # échantillon de la deuxième loi
+quantile(x + y, 0.95)      # 95e centile de la convolution
+
+## Il est laissé en exercice de faire le même calcul avec Excel.
